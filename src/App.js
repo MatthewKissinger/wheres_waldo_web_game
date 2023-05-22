e5c84aecc23cfd1bd9357b1bbabc93e5aef8e44d
Matt <matthew.t.kissinger@gmail.com>
2023-05-22 11:09:27 -0700

    added Main.js component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import './styles/App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      App here
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </div>
   );
 }
