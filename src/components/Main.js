a11a0f847c2c661a8ec38078d4fef79a800e414f
Matt <matthew.t.kissinger@gmail.com>
2023-05-22 11:19:04 -0700

    imported image for first game and set static sizing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import hoth_battle from "../assets/images/wheres_wookie_ryan_hill.jpeg"
+import "../styles/Main.css"
 
 export default function Main() {
     return (
         <div>
             Main content here
+            <img class="game-image" src={hoth_battle} alt='hoth battle scene'></img>
         </div>
     )
 }
