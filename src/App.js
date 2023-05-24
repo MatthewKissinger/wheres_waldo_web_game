ea418b9527b33368c54c3abbab1405c5c6eb90a7
Matt <matthew.t.kissinger@gmail.com>
2023-05-24 16:04:18 -0700

    grabCoordinates function in progress

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
+// store all of the state and functions inside of App.js
+
+// TODO
+
+// 1) find the coordinates of 3 characters in the image
+//  -- create a grab coordinate function for the image
+//  -- console.log the coordinate clicked 
+// 2) save these coordinates in a firestore database
+// 3) when the user clicks on the image a selection circle should appear -- that circle will grab an area of pixels from the image 
 
 function App() {
+
+  // methods
+  function grabCoordinate(e) {
+
+    let xOffSet = e.target.offsetLeft;
+    let yOffSet = e.target.offsetTop;
+
+    console.log(`X: ${e.pageX - xOffSet}`);
+    console.log(`Y: ${e.pageY - yOffSet}`);
+  }
+
+
   return (
     <div className="App">
       <Header></Header>
-      <Main></Main>
+      <Main
+        grabCoordinate={grabCoordinate}
+      ></Main>
       <Footer></Footer>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,17 @@ import React from 'react';
 import hoth_battle from "../assets/images/wheres_wookie_ryan_hill.jpeg"
 import "../styles/Main.css"
 
-export default function Main() {
+export default function Main(props) {
+    const { grabCoordinate } = props;
+
     return (
         <div className='main'>
             <div className='image__container'>
-                <img class="image" src={hoth_battle} alt='hoth battle scene'></img>
+                <img className="image" 
+                    src={hoth_battle} 
+                    alt='hoth battle scene'
+                    onMouseDown={grabCoordinate}
+                ></img>
             </div>  
         </div>
     )
