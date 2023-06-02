255488d0488107ef47efb3a4b11bb655b8851187
Matt <matthew.t.kissinger@gmail.com>
2023-06-01 21:34:55 -0700

    added pointer events for the image and the li's p's for the dropdown selections

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ import Footer from './components/Footer';
 
 // TODO
 // 1) create a dropdown menu that displays to the right of the selection circle after the user makes a selection by clicking on the image
-//  -- this dropdown is to include portraits of the characters that are clickable as an option
+//  -- this dropdown is to include portraits of the characters that are clickable as an option-- DONE
 
-// 2) save the ratios of pixel location for each character in a firestore database using the grabCoordinate function
+// 2) make the image unclickable after a selection on the image has been made.  After the user selects an option from the dropdown menu make the image clickable again until the game is completed
+
+// 3) save the ratios of pixel location for each character in a firestore database using the grabCoordinate function
 
 // store image width and height in state -- update every time the window is resized
 
@@ -18,43 +20,63 @@ function App() {
 
   // state
 
+  // image styling -- value is for the pointer-events styling property
+  const [ imageStyle, setImageStyle ] = useState('auto');
+
   // the user target circle location
   // first array value = top, second = left, third = visibility
   const [ targetLocation, setTargetLocation] = useState(['50px', '50px', 'hidden']);
+
+  // the selection dropdown box after a user clicks on the image
+  const [ dropdownLocation, setDropdownLocation ] = useState(['50px', '50px', 'hidden']);
 
   // methods
   function grabCoordinateRatio(e) {
     // ratios grabbed for characters to be stored for reference from user clicks
 
+    // get the target's dimensions
+    let offset = e.target.getBoundingClientRect();
+
     // get imageWidth
     let imageWidth = e.target.clientWidth;
 
     // get xCoordinate of mouseclick inside of the image
-    let xOffSet =  e.target.offsetLeft;
+    let xOffSet =  offset.left;
     let xCoordinate = e.pageX - xOffSet;
     let xRatio = xCoordinate / imageWidth;
-    console.log(`X Ratio: ${xRatio.toFixed(3)}`);
+    // console.log(`X Ratio: ${xRatio.toFixed(3)}`);
 
     // get imageHeight
     let imageHeight = e.target.clientHeight;
 
     // get xCoordinate of mouseclick inside of the image
-    let yOffSet = e.target.offsetTop;
+    let yOffSet = offset.top;
     let yCoordinate = e.pageY - yOffSet;
     let yRatio = yCoordinate / imageHeight;
-    console.log(`Y Ratio: ${yRatio.toFixed(3)}`);
+    // console.log(`Y Ratio: ${yRatio.toFixed(3)}`);
   }
 
   function showTargetingBox(e) {
-    // console.log(e.target);
     let offset = e.target.getBoundingClientRect();
 
     let xCoordinate = (e.pageX - offset.left) - 40;
 
     let yCoordinate = ((e.pageY - offset.top) - 40) - window.scrollY;
 
-    // update the state of targeting box on click -> conditionally render it in Main.js component
+    // update the state of targeting box on click
     setTargetLocation([`${yCoordinate.toString()}px`, `${xCoordinate.toString()}px`, 'visible']);
+
+    let dropDownXCoordinate = xCoordinate + 100;
+
+    // the state of the dropdownLocation on click
+    setDropdownLocation([`${yCoordinate.toString()}px`, `${dropDownXCoordinate.toString()}px`, 'visible'])
+
+    // the state of the pointer-events on the image
+    setImageStyle('none');
+  }
+
+  function selectDropdownOption(e) {
+    console.log(e.target);
   }
 
   return (
@@ -63,7 +85,10 @@ function App() {
       <Main
         grabCoordinateRatio={grabCoordinateRatio}
         showTargetingBox={showTargetingBox}
+        selectDropdownOption={selectDropdownOption}
+        imageStyle={imageStyle}
         targetLocation={targetLocation}
+        dropdownLocation={dropdownLocation}
       ></Main>
       <Footer></Footer>
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import hoth_battle from "../assets/images/wheres_wookie_ryan_hill.jpeg"
 import "../styles/Main.css"
+import Boba from "../assets/images/Boba-Fett-Portrait.jpg"
+import Chewbacca from "../assets/images/Chewbacca-Portrait.jpg"
+import Greedo from "../assets/images/Greedo-Portrait.jpg"
 
 export default function Main(props) {
-    const { grabCoordinateRatio, showTargetingBox, targetLocation } = props;
+    const { grabCoordinateRatio, showTargetingBox, selectDropdownOption, imageStyle, targetLocation, dropdownLocation } = props;
 
     return (
         <div className='main'>
             <div className='image__container'
-                onClick={showTargetingBox}
             >
-                <img className="image" 
+                <img className="image"
+                    onClick={showTargetingBox}
+                    style={{'pointerEvents': `${imageStyle}`}} 
                     src={hoth_battle} 
                     alt='hoth battle scene'
                     onMouseDown={grabCoordinateRatio}
@@ -20,10 +24,24 @@ export default function Main(props) {
                             'left': `${targetLocation[1]}`,
                             'visibility': `${targetLocation[2]}`}}
                 ></div>
-                <div className='dropdown'>
-                    <li className='dropdown_char1'>Chewbacca</li>
-                    <li className='dropdown_char2'>Boba Fett</li>
-                    <li className='dropdown_char3'>Greedo</li>
+                <div className='dropdown'
+                    style={{'top': `${dropdownLocation[0]}`, 
+                            'left': `${dropdownLocation[1]}`,
+                            'visibility': `${dropdownLocation[2]}`}}
+                    onClick={selectDropdownOption}
+                >
+                    <li className='dropdown_char1' id='char1'>
+                        <img src={Chewbacca} alt='portrait of Chewbacca'></img>
+                        <p>Chewbacca</p>
+                    </li>
+                    <li className='dropdown_char2' id='char2'>
+                        <img src={Boba} alt='portrait of Boba Fett'></img>
+                        <p>Boba Fett</p>
+                    </li>
+                    <li className='dropdown_char3' id='char3'>
+                        <img src={Greedo} alt='portrait of Greedo'></img>
+                        <p>Greedo</p>
+                    </li>
                 </div>
             </div>  
         </div>
