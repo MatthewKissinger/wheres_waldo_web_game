fad58b865b129aa9a023cfdd199fd20d1219d4d7
Matt <matthew.t.kissinger@gmail.com>
2023-06-18 15:22:49 -0700

    updated Timer.js to use Date.now() functionality

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,9 +1,37 @@
+import { useRef, useState } from 'react';
 import '../styles/Timer.css'
 
-export default function Timer() {
+export default function Timer(props) {
+
+    // pass into Timer the user prop that will have a gamestart value of true, if the gamestart value is true the timer will begin
+
+    const [time, setTime] = useState(null);
+    const [now, setNow] = useState(null);
+ 
+    const intervalRef = useRef(null);
+
+    function timerStart() {
+        setTime(Date.now());
+        setNow(Date.now());
+
+        intervalRef.current = setInterval(() => {
+            setTime(Date.now())
+        }, 10);
+    }
+
+    function timerClear() {
+        clearInterval(intervalRef.current);
+    }
+
+    let timePassed = (time - now) / 1000;
+
+
     return (
         <div className="timer">
-            <p>Timer: 00:00:000</p>
+            <p>Timer: {timePassed.toFixed(3)}</p>
+            <button className='timer-start'
+                    onClick={timerStart}
+            >start</button>
         </div>
     )
 }
