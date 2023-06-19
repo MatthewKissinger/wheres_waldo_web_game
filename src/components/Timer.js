98ec243089824c8a849649bccc463e5e52504878
Matt <matthew.t.kissinger@gmail.com>
2023-06-19 16:23:31 -0700

    stopwatch working on the header

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,30 +5,34 @@ export default function Timer(props) {
 
     // pass into Timer the user prop that will have a gamestart value of true, if the gamestart value is true the timer will begin
 
-    const [time, setTime] = useState(null);
-    const [now, setNow] = useState(null);
- 
-    const intervalRef = useRef(null);
+    const [time, setTime] = useState({ms:0, s:0, m:0});
+    
+    let updatedMs = time.ms, updatedS = time.s, updatedM = time.m;
+
+    const runTimer = () => {
+        if (updatedS === 60) {
+            updatedM++;
+            updatedS = 0;
+        }
+        if (updatedMs === 100) {
+            updatedS++;
+            updatedMs = 0;
+        }
+        updatedMs++;
+        return setTime({ms:updatedMs, s:updatedS, m:updatedM});
+    }
 
     function timerStart() {
-        setTime(Date.now());
-        setNow(Date.now());
-
-        intervalRef.current = setInterval(() => {
-            setTime(Date.now())
-        }, 10);
+        runTimer();
+        setInterval(runTimer, 10);
     }
-
-    function timerClear() {
-        clearInterval(intervalRef.current);
-    }
-
-    let timePassed = (time - now) / 1000;
-
 
     return (
         <div className="timer">
-            <p>Timer: {timePassed.toFixed(3)}</p>
+            <p>Timer:</p>
+            <span>{(time.m >= 10) ? time.m : "0" + time.m}</span>&nbsp;:&nbsp;
+            <span>{(time.s >= 10) ? time.s : "0" + time.s}</span>&nbsp;:&nbsp;
+            <span>{(time.ms >= 10) ? time.ms : "0" + time.ms}</span>
             <button className='timer-start'
                     onClick={timerStart}
             >start</button>
